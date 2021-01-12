4591ddb13ad637b09358f98e0218537bb8fdc069
Alexander <afernandez36@academic.rrc.ca>
2021-01-12 14:18:47 -0600

    second init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      Hi
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      todo: [],
+      textInput: ""
+    };
+  }
+
+  handleOnChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleKeyPress = (e) =>{
+    if(e.keyCode === 13){
+      //
+    }
+  };
+
+  render(){
+    return(
+      <div>
+        <h1 className="center">To-Do List</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
